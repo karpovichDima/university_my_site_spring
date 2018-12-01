9532ff752548c422766a1b614c00c69479e02ba3
Karpovich Dima <karpovichdi@hotmail.com>
2018-12-01 22:02:47 +0300

    connected css to thymeleaf

--- 1.1/src/main/java/com/university/GreetingController.java
+++ 1.1/src/main/java/com/university/GreetingController.java
@@ -4,31 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
-
-//    @GetMapping()
-//    public String test(@RequestParam() String name, Model model) {
-//        model.addAttribute("name", name);
-//        return "greeting";
-//    }
-
     @GetMapping("/signup")
     public String getTextFromForm(@ModelAttribute CreateUserForm userForm, Model model) {
+        if (userForm.getEmail() == null) userForm.setEmail("default@email.com");
+        if (userForm.getName() == null) userForm.setEmail("Anonimous");
         User user = CreateUserFormToUser.convert(userForm);
         String email = user.getEmail();
         String name = user.getName();
         model.addAttribute("email", email);
         model.addAttribute("name", name);
-        return "greeting";
+        return "index";
     }
-
 }
